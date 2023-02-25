4b54434d42b6d8570e4e541cae14d8c4a99f948e
meiliou <83768277+meiliou@users.noreply.github.com>
2023-02-25 21:55:38 +0200

    start, Q&A views, validated answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,39 @@
+var questionBank = [
+    {   
+        question: "How does a WHILE loop start?",
+        options: ["while i = 1 to 10", "while (i <= 10; i++)", "while (var i = 0; i = 10; i++)", "while (i <= 10)"],
+        answer: "while (i <= 10)"
+    },
+    {   
+        question: "The external JavaScript file must contain the <script> tag.",
+        options: ["True", "False", "Maybe", "I don't know"],
+        answer: "False"
+    },
+    {   
+        question: "Inside which HTML element do we put the JavaScript?", 
+        options: ["<script>", "<js>", "<javascript>", "<JS>"],
+        answer: "<script>"
+    }
+];
+
 var sectionWelcomeEl = document.querySelector(".section-welcome");
 var sectionQuestionsEl = document.querySelector(".section-questions");
 var sectionInputEl = document.querySelector(".section-input");
 var sectionHighscoreEl = document.querySelector(".section-highscore");
 var startButtonEl = document.querySelector("#start-button");
-
 var countdownEl = document.querySelector("#countdown");
+var questionsEl = document.querySelector(".questions");
+var answersEl = document.querySelector(".answers");
+var optionAEl = document.querySelector("#optionA");
+var optionBEl = document.querySelector("#optionB");
+var optionCEl = document.querySelector("#optionC");
+var optionDEl = document.querySelector("#optionD");
+var feedbackEl = document.querySelector(".feedback");
+var timeLeft = questionBank.length * 15;
+var q = 0;
+score = 0;
+
+
 
 
 // Starting view
@@ -16,8 +45,6 @@ sectionHighscoreEl.style.display='none';
 
 // Countdown
 function countdown() {
-    var timeLeft = 5;
-
     var timeInterval = setInterval(function(){
         if (timeLeft>=0) {
             countdownEl.textContent = "Time left: " + timeLeft;
@@ -26,6 +53,40 @@ function countdown() {
     }, 1000);
 };
 
+// Display Q&A
+function displayQA() {
+    // if (q < questionBank.length) {
+        questionsEl.textContent = questionBank[q].question;
+        optionAEl.textContent = questionBank[q].options[0];
+        optionBEl.textContent = questionBank[q].options[1];
+        optionCEl.textContent = questionBank[q].options[2];
+        optionDEl.textContent = questionBank[q].options[3];
+    // } else {
+        // "gameover placeholder"
+    // }  
+}
+// Validating if answer is right or wrong
+var compareAnswer = function(event) {
+    if (q >= questionBank.length) {
+        window.alert("gameOver");
+        clearInterval(timeInterval);
+    } else if (event === questionBank[q].answer) {
+        feedbackEl.textContent="Correct!";
+    } else {
+        timeLeft=timeLeft-10;
+        feedbackEl.textContent="You are wrong!";
+    }
+    score = timeLeft;
+    q++;
+    displayQA();
+}
+var compare = function (event) {
+    var event = event.target;
+    compareAnswer(event.textContent.trim());
+}
+answersEl.addEventListener("click", compare);
+
+
 // Game starts
 var startGame = function() {
     sectionWelcomeEl.style.display='none';
@@ -33,9 +94,12 @@ var startGame = function() {
     sectionInputEl.style.display='none';
     sectionHighscoreEl.style.display='none';
     countdown();
+    displayQA();
 }
 
-// Game starts on click
+
+
+// Event-Listener Game starts on click
 startButtonEl.addEventListener("click", startGame);
 
 
